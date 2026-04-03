d2ab0468665c2010c15ca854545312fea9cc291c
Imam Hossan <imamhossan9932@gmail.com>
2026-04-03 12:57:07 +0600

    ADDED

--- index.js
+++ index.js
@@ -32,6 +32,18 @@ async function run() {
     // parcel api
     app.get('/parcels',async(req,res)=>{
 
+      const query={};
+
+const {email}=req.query;
+// parcels? email="" &
+if(email){
+  query.senderEmail=email;
+}
+
+      const cursor=parcelsCollection.find(query);
+      const result=await cursor.toArray();
+      res.send(result);
+
     });
 
     app.post('/parcels',async(req,res)=>{
