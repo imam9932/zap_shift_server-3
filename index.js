f241f86face4c2b11d2e83e12bb7aeafc9743b40
Imam Hossan <imamhossan9932@gmail.com>
2026-04-10 23:42:12 +0600

    added

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const app = express()
- const cors=require('cors');
+const cors = require('cors');
+const dns = require('dns');
+
+//  change DNS
+dns.setServers(["1.1.1.1", "8.8.8.8"])
 
 
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port =process.env.PORT || 3000;
+// ✅ middleware 
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 3000;
 
 
- 
+
 const uri = "mongodb+srv://new_zap_shift_DB:djLzjUkM19OxDMJW@cluster0.msoxyyg.mongodb.net/?appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,30 +34,38 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const db=client.db('zap_shift_db_3');
-    const parcelsCollection=db.collection('parcels');
+    const db = client.db('zap_shift_db_3');
+    const parcelsCollection = db.collection('parcels');
 
     // parcel api
-    app.get('/parcels',async(req,res)=>{
+    app.get('/parcels', async (req, res) => {
+      const query = {};
+      const { email } = req.query;
+      // parcels? email="" &
+      if (email) {
+        query.senderEmail = email;
+      }
 
-      const query={};
-
-const {email}=req.query;
-// parcels? email="" &
-if(email){
-  query.senderEmail=email;
-}
-
-      const cursor=parcelsCollection.find(query);
-      const result=await cursor.toArray();
+      const options={sort : {createdAt:-1}}
+      const cursor = parcelsCollection.find(query,options);
+      const result = await cursor.toArray();
       res.send(result);
 
     });
 
-    app.post('/parcels',async(req,res)=>{
-      const parcel=req.body;
-      const result= await parcelsCollection.insertOne(parcel)
+    app.post('/parcels', async (req, res) => {
+      const parcel = req.body;
+      // parcel created time
+      parcel.createdAt = new Date();
+      const result = await parcelsCollection.insertOne(parcel)
       res.send(result)
+    });
+
+    app.delete('/parcels/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)};
+      const result=await parcelsCollection.deleteOne(query);
+      res.send(result);
     })
 
 
